d4fce65823c50590ad722d4c63c0ae90e6b13008
Rudreshcg <rudreshcsbg82@gmail.com>
2026-09-12 20:14:12 +0530

    fix: update default contact endpoint URL in contactService.js

--- src/utils/contactService.js
+++ src/utils/contactService.js
@@ -1,5 +1,5 @@
 const DEFAULT_CONTACT_ENDPOINT =
-  "https://script.google.com/macros/s/AKfycbxq6xuzj7CK4YI9N91iZIGro1QKem75BJIhbnPxN6IiLSPt1qXl1N1Pfr3stM_9W8U4HQ/exec";
+  "https://script.google.com/macros/s/AKfycbzpdu8gSF-L6zfCIvrJ56R9kNn4Nd4kz9OQ-15EScQ_NToxc_ASRGMvOJX-GfQ0JViF9A/exec";
 
 const getContactEndpoint = () => {
   const url = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || DEFAULT_CONTACT_ENDPOINT;
